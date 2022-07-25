2c404fc1bb43dd368ca8f271c8203a805d2b9c52
A B M Shawon Islam <abmshawonislam@gmail.com>
2022-07-25 12:33:25 +0600

    friend Request

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { getDatabase,set , ref, onValue,push} from "firebase/database";
 import { getAuth } from "firebase/auth";
+import { BsCheck } from 'react-icons/bs';
 const UserList = () => {
     const auth = getAuth();
     const db = getDatabase();
     // console.log(auth.currentUser)
     
 let [userlist,setUserlist] = useState([])
+let [friendRequest,setFriendRequest] = useState([])
+let [friendRequest2,setFriendRequest2] = useState([])
+let [change,setChange] = useState(false)
 
 useEffect(()=>{
         let userArr = []
@@ -26,12 +30,35 @@ useEffect(()=>{
         });
     },[])
 
+    useEffect(()=>{
+        let friendRequestArr = []
+        let friendRequestArr2 = []
+        const friendRquestRef = ref(db, 'friendrequest/');
+        onValue(friendRquestRef, (snapshot) => {
+            snapshot.forEach((item)=>{
+                console.log("item.receiverid",item.receiverid)
+                console.log("auth.currentUser.uid",auth.currentUser.uid)
+             
+                friendRequestArr.push(item.val().receiverid)
+                friendRequestArr2.push(item.val().senderid)
+            
+                
+                
+            })
+            setFriendRequest(friendRequestArr)
+            setFriendRequest2(friendRequestArr2)
+        });
+    },[change])
+    console.log(friendRequest2)
+
+
     let handleFriendRequest = (info)=>{
         set(push(ref(db, 'friendrequest')), {
             name: auth.currentUser.displayName,
             receiverid: info.id,
             senderid: auth.currentUser.uid
         });
+        setChange(!change)
     }
 
   return (
@@ -48,9 +75,17 @@ useEffect(()=>{
                     <h1>{item.username}</h1>
                     <h4>{item.email}</h4>
                 </div>
+                {friendRequest.includes(item.id) && friendRequest2.includes(auth.currentUser.uid) || friendRequest.includes(auth.currentUser.uid) && friendRequest2.includes(item.id) ?
+                
+                
+                <div className='button'>
+                    <button><BsCheck/></button>
+                </div>
+                :
                 <div className='button'>
                     <button onClick={()=>handleFriendRequest(item)}>+</button>
                 </div>
+                }
             </div>
         ))}
         
